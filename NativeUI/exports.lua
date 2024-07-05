d6fd2eae546836281d823295ac39bce94200089f
Tomasz Zajac <tomezpl@gmail.com>
2024-07-05 19:32:18 +0100

    feat: started adding JS wrapper
    # Conflicts:
    #	MenuExample/MenuExample.lua
    #	MenuExample/__resource.lua
    #	NativeUI/MenuExample.lua
    #	NativeUI/MenuPool.lua
    #	NativeUI/NativeUI.lua
    #	NativeUI/UIMenu.lua
    #	NativeUI/Utils.lua
    #	NativeUI/__resource.lua
    #	NativeUI/elements/Badge.lua
    #	NativeUI/elements/Colours.lua
    #	NativeUI/elements/Sprite.lua
    #	NativeUI/elements/StringMeasurer.lua
    #	NativeUI/elements/UIResRectangle.lua
    #	NativeUI/elements/UIResText.lua
    #	NativeUI/items/UIMenuCheckboxItem.lua
    #	NativeUI/items/UIMenuColouredItem.lua
    #	NativeUI/items/UIMenuItem.lua
    #	NativeUI/items/UIMenuListItem.lua
    #	NativeUI/items/UIMenuSliderItem.lua
    #	NativeUI/panels/UIMenuGridPanel.lua
    #	NativeUI/panels/UIMenuPercentagePanel.lua
    #	README.md

---
+++ NativeUI/exports.lua
@@ -0,0 +1,29 @@
+-- exports("NativeUI", NativeUI)
+
+-- Since I can't figure out a way to pass objects with functions between JS and Lua,
+-- we'll store the actual pool and menu objects in here and just return indices to the JS.
+pools = {}
+poolCount = 0
+menus = {}
+menuCount = 0
+
+exports("CreatePool", function()
+    table.insert(pools, NativeUI.CreatePool())
+    poolCount = poolCount + 1
+    return poolCount
+end)
+exports("CreateMenu", function(name, colour, width, height)
+    table.insert(menus, NativeUI.CreateMenu(name, colour, width, height))
+    menuCount = menuCount + 1
+    return menuCount
+end)
+exports("MenuPool.Add", function(menuPool, menu)
+    pools[menuPool]:Add(menus[menu])
+end)
+exports("MenuPool:ProcessMenus", function (menuPool)
+    pools[menuPool]:ProcessMenus()
+end)
+
+exports("Menu:Visible", function (menu, visible)
+    menus[menu]:Visible(visible)
+end)
